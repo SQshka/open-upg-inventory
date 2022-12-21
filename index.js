a03454a79727531092da822293cea03468f22648
SQshka <shukepc@gmail.com>
2022-12-21 20:14:03 +0300

    fixed some mirror bugs, added some of videos

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function(){
         particlesJS("snow", {
             "particles": {
                 "number": {
-                    "value": 100,
+                    "value": 50,
                     "density": {
                         "enable": true,
                         "value_area": 800
